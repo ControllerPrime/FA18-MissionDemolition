5c5133838c134cc200a308f9fae02e916b79cf87
ControllerPrime <31867665+ControllerPrime@users.noreply.github.com>
2018-09-18 18:47:53 -0400

    Cloud
    Created clouds and cloud scripts

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -7,6 +7,7 @@ public class FollowCam : MonoBehaviour {
 
     [Header("Set in Insepector")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Set Dynamically")]
     public float camZ;
@@ -21,8 +22,11 @@ public class FollowCam : MonoBehaviour {
         if (POI == null) return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
